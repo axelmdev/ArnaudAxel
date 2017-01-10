1f616a29dfcc6cff146cd0f02e61e3a05770602f
Axel Mainguy <killerbossx@gmail.com>
2017-01-10 15:50:51 +0100

    Ajout de la classe User et du .gitlog

---
+++ src/entities/User.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package entities;
+
+/**
+ * @author Axel
+ *
+ */
+public class User {
+	public String nom;
+	public String prenom;
+	public String login;
+	public String mot_de_passe;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getMot_de_passe() {
+		return mot_de_passe;
+	}
+	public void setMot_de_passe(String mot_de_passe) {
+		this.mot_de_passe = mot_de_passe;
+	}
+	
+}
