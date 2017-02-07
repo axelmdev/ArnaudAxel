a3c4737d75b400392ec5763f31c8533691f64686
Maurice Arnaud <arnaud.maurice.pro@gmail.com>
2017-02-07 16:38:07 +0100

    Clear jsonManager avant manipulation

--- src/entities/ConfigurationManager.java
+++ src/entities/ConfigurationManager.java
@@ -8,6 +8,7 @@ public class ConfigurationManager {
 	public static void main(String[] args) {
 		JsonManager json = JsonManager.getInstance();		
 		
+			json.clear();
 			Class Configuration = null;
 			json.readFromFile("conf.prog", "/ArnaudAxel/conf.prog", Configuration);
 
